9feeec0c1c2c4867e4a246e4de0dfb73a4843b70
t-isihmaru <t-ishimaru@dip-net.co.jp>
2017-11-29 11:01:01 +0900

    ボーダーの処理をコメントアウト

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 function trackLocation(e) {
-	var rect = imageContainer.getBoundingClientRect(),
+	var rect = imageContainer.getBoundingClientRect(), //座標を取得
 		position = ((e.pageX - rect.left) / imageContainer.offsetWidth)*100;
 	if (position <= 100) {
-		imageClipper.style.width = position+"%";
-    // clippedimage.style.width = ((100/position)*100)+"%";
-    // clippedimage.style.zIndex = 3;
+		imageClipper.style.width = position+"%"; //幅を決める
 	}
 }
-var imageContainer = document.getElementById("image-compare-container"),
-imageClipper = document.getElementById("image-clipper"),
+var imageContainer = document.getElementById("image-compare-container"), //下にいる画像
+imageClipper = document.getElementById("image-clipper"), //クリッピングする画像
 clippedimage = imageClipper.getElementsByTagName("image")[0];
-imageContainer.addEventListener( "mousemove", trackLocation, false);
+imageContainer.addEventListener( "mousemove", trackLocation, false); //イベントリスナーを登録
 imageContainer.addEventListener("touchstart",trackLocation,false);
 imageContainer.addEventListener("touchmove",trackLocation,false);
+
+// var elements = document.getElementsByClassName("photoslider__border"); //ボーダーの要素を取得

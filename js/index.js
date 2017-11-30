f9f6bcb3cb86539c4ff9d323c31db6041a9f50c7
t-isihmaru <t-ishimaru@dip-net.co.jp>
2017-11-30 12:32:56 +0900

    classNameに変更する

--- js/index.js
+++ js/index.js
@@ -1,15 +1,14 @@
+var imageContainer = document.getElementsByClassName("image-compare-container"),
+		imageClipper = document.getElementsByClassName("image-clipper"),
+		clippedimage = imageClipper[0].getElementsByTagName("img");
+
 function trackLocation(e) {
-	var rect = imageContainer.getBoundingClientRect(), //座標を取得
-		position = ((e.pageX - rect.left) / imageContainer.offsetWidth)*100;
+	var rect = imageContainer[0].getBoundingClientRect(),
+			position = ((e.pageX - rect.left) / imageContainer.offsetWidth)*100;
 	if (position <= 100) {
-		imageClipper.style.width = position+"%"; //幅を決める
+			imageClipper[0].style.width = position+"%";
 	}
 }
-var imageContainer = document.getElementById("image-compare-container"), //下にいる画像
-imageClipper = document.getElementById("image-clipper"), //クリッピングする画像
-clippedimage = imageClipper.getElementsByTagName("image")[0];
-imageContainer.addEventListener( "mousemove", trackLocation, false); //イベントリスナーを登録
-imageContainer.addEventListener("touchstart",trackLocation,false);
-imageContainer.addEventListener("touchmove",trackLocation,false);
-
-// var elements = document.getElementsByClassName("photoslider__border"); //ボーダーの要素を取得
+imageContainer[0].addEventListener("mousemove", trackLocation);
+imageContainer[0].addEventListener("touchstart",trackLocation);
+imageContainer[0].addEventListener("touchmove",trackLocation);
